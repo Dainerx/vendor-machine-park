59dcc42b8a6b0f9340d98b71a87b2473645203ce
Dainerx <d.oussamabenghorbel@gmail.com>
2020-02-22 17:35:48 +0100

    fix

--- src/servlets/AutomateServlet.java
+++ src/servlets/AutomateServlet.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AutomateServlet
  */
-@WebServlet("/automateservlet")
+@WebServlet("/")
 public class AutomateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
